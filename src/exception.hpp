d4c86cb690bb2144dc10577a2b0c813c0f6dd477
nolan <nolan@9e3857e0-fc33-0410-b652-94ab0348a082>
2004-11-01 21:34:58 +0000

    [project @ 2004-11-01 21:34:58 by nolan] Changed the header
    git-svn-id: file:///home/svn/MyPasswordSafe/trunk@131 9e3857e0-fc33-0410-b652-94ab0348a082

--- src/exception.hpp
+++ src/exception.hpp
@@ -1,4 +1,20 @@
-/* $Header: /home/cvsroot/MyPasswordSafe/src/exception.hpp,v 1.2 2004/05/04 22:48:44 nolan Exp $
+/* $Header: /home/cvsroot/MyPasswordSafe/src/exception.hpp,v 1.3 2004/11/01 21:34:58 nolan Exp $
+ * Copyright 2004, Semantic Gap (TM)
+ * http://www.semanticgap.com/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #ifndef _EXCEPTION_HPP_
 #define _EXCEPTION_HPP_
--- src/safelistview.hpp
+++ src/safelistview.hpp
@@ -1,4 +1,20 @@
-/* $Header: /home/cvsroot/MyPasswordSafe/src/safelistview.hpp,v 1.11 2004/10/04 03:04:12 nolan Exp $
+/* $Header: /home/cvsroot/MyPasswordSafe/src/safelistview.hpp,v 1.12 2004/11/01 21:34:58 nolan Exp $
+ * Copyright (c) 2004, Semantic Gap (TM)
+ * http://www.semanticgap.com/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #ifndef SAFELISTVIEW_HPP
 #define SAFELISTVIEW_HPP
