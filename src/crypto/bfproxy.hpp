306c77933c56e377b5fa9793e68bd41b08ba761f
nolan <nolan@9e3857e0-fc33-0410-b652-94ab0348a082>
2004-12-06 13:07:04 +0000

    [project @ 2004-12-06 13:07:03 by nolan] Added copyright notice
    git-svn-id: file:///home/svn/MyPasswordSafe/trunk@159 9e3857e0-fc33-0410-b652-94ab0348a082

--- src/crypto/bfproxy.hpp
+++ src/crypto/bfproxy.hpp
@@ -1,3 +1,21 @@
+/* $Header: /home/cvsroot/MyPasswordSafe/src/crypto/bfproxy.hpp,v 1.2 2004/12/06 13:07:04 nolan Exp $
+ * Copyright (c) 2004, Semantic Gap (TM)
+ * http://www.semanticgap.com/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 #ifndef BFPROXY_HPP
 #define BFPROXY_HPP
 
